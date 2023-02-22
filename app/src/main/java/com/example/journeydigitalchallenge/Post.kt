9bf67ec40d0ee86d29c425d2d77451b54dbc9aad
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-02-23 03:21:16 +1300

    Setup: gradle config. for parcelize
    plugins {
        ....
        id "org.jetbrains.kotlin.plugin.parcelize"
    }

--- app/src/main/java/com/example/journeydigitalchallenge/Post.kt
+++ app/src/main/java/com/example/journeydigitalchallenge/Post.kt
@@ -1,8 +1,11 @@
 package com.example.journeydigitalchallenge
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Post(
     val userId: Int,
     val id: Int,
     val title: String,
     val body: String
-)
+): Parcelable
