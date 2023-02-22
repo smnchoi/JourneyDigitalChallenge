98378347f195044c346dcd3574126958bec6e269
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-02-22 21:31:51 +1300

    Chore: make files for data class

--- app/src/main/java/com/example/journeydigitalchallenge/PostScreen.kt
+++ app/src/main/java/com/example/journeydigitalchallenge/PostScreen.kt
@@ -115,21 +115,6 @@ fun PostScreen() {
     }
 }
 
-data class Post(
-    val userId: Int,
-    val id: Int,
-    val title: String,
-    val body: String
-)
-
-data class Comment(
-    val postId: Int,
-    val id: Int,
-    val name: String,
-    val email: String,
-    val body: String
-)
-
 @Composable
 fun PostDetail(
     post: Post,
